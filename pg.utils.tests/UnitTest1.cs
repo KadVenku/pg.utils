b52fd7d34d3c69f6067c6fa843a58cb1d56c737a
Kad_Venku <venkukad@gmail.com>
2018-12-27 14:44:07 +0100

    Init.

---
+++ pg.utils.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace pg.utils.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
