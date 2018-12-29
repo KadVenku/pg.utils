2124b152e463439d76d91f24955ce50872c376f0
Kad_Venku <venkukad@gmail.com>
2018-12-29 20:50:09 +0100

    Added float utility, added unit tests.

--- pg.util.tests/FloatUtility.Test.cs
+++ pg.util.tests/FloatUtility.Test.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace pg.util.tests
@@ -5,6 +6,45 @@ namespace pg.util.tests
     [TestClass]
     public class FloatUtilityTest
     {
+        [TestMethod]
+        [DataRow("\t1.234\n\t")]
+        [DataRow("1.234")]
+        [DataRow("\t2.345f    ")]
+        public void ParseStringToFloatNotNull(string s)
+        {
+            float val = FloatUtility.Parse(s);
+            Assert.IsTrue(val > 0.0f || val < 0.0f);
+        }
+
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow("   ")]
+        [DataRow("\t\n\t\t   \t")]
+        public void ParseStringToFloatNull(string s)
+        {
+            float val = FloatUtility.Parse(s);
+            Assert.IsTrue(val == 0.0f);
+        }
+
+        [TestMethod]
+        [DataRow(0.0f)]
+        [DataRow(1.2345f)]
+        [DataRow(1.5678f)]
+        public void ParseFloatToStringNoDecimal(float f)
+        {
+            string val = FloatUtility.Parse(f);
+            Assert.IsTrue(val == "0" || val =="1" || val=="2");
+        }
         
+        [TestMethod]
+        [DataRow(0.0f)]
+        [DataRow(1.2345f)]
+        [DataRow(1.5678f)]
+        public void ParseFloatToStringTwoDecimal(float f)
+        {
+            string val = FloatUtility.Parse(f, 2);
+            Assert.IsTrue(val == "0.00" || val =="1.23" || val=="1.57");
+        }
     }
 }
--- pg.util/FloatUtility.cs
+++ pg.util/FloatUtility.cs
@@ -2,23 +2,36 @@
 
 namespace pg.util
 {
+    /// <summary>
+    /// A convenience class to handle Petroglyph's float types.
+    /// </summary>
     public class FloatUtility
     {
         private static readonly CultureInfo INV_CULTURE_INFO = CultureInfo.GetCultureInfo("en-US");
+        /// <summary>
+        /// Parses the specified string as float value.
+        /// </summary>
+        /// <param name="floatValue">The string that should be parsed as float value.</param>
+        /// <returns></returns>
         public static float Parse(string floatValue)
         {
             if (string.IsNullOrEmpty(floatValue) || string.IsNullOrWhiteSpace(floatValue))
             {
                 return 0.0f;
             }
+            floatValue = floatValue.Replace('f', ' ');
+            floatValue = floatValue.Replace('F', ' ');
+            floatValue = floatValue.Trim();
             return float.Parse(floatValue, INV_CULTURE_INFO);
         }
 
-        public static string Parse(float floatValue)
-        {
-            return floatValue.ToString(INV_CULTURE_INFO);
-        }
-        public static string Parse(float floatValue, int decimalPlaces)
+        /// <summary>
+        /// Parses the specified float value that should be parsed as string.
+        /// </summary>
+        /// <param name="floatValue">The float value.</param>
+        /// <param name="decimalPlaces">The decimal places.</param>
+        /// <returns></returns>
+        public static string Parse(float floatValue, int decimalPlaces = 0)
         {
             return floatValue.ToString("F" + decimalPlaces, INV_CULTURE_INFO);
         }
